9c936fc743142d07523680fd7dc9b5e02eb0d5da
Nicolas <u20241D428@upc.edu.pe>
2026-04-08 10:53:17 -0500

    feat(greeting): add GET and POST endpoints for greetings with developer handling

--- Acme.Hello.Platform/Program.cs
+++ Acme.Hello.Platform/Program.cs
@@ -1,3 +1,6 @@
+using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Assemblers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +10,24 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
+
+app.MapGet(pattern: "greetings", (string? firstName, string? lastnName) =>
+{
+    var developer = !string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastnName)
+        ? new Developer(firstName, lastnName)
+        : null;
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Ok(response);
+}).WithName("GetGreeting");
+
+app.MapPost(pattern: "/greetings", (GreetDeveloperRequest request) =>
+{
+    var developer = DeveloperAssembler.ToEntityFromRequest(request);
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Ok(response);
+}).WithName("PostGreeting");
 
 app.Run();
